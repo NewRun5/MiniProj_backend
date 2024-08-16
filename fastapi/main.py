02cd0cfa8402d16815beb11f9dbd05ea5e0f0b99
ahmin030 <ahmin030@naver.com>
2024-08-16 16:47:17 +0900

    Fix: 반환값 수정

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from pydantic import BaseModel
 from PIL import Image
 import numpy as np
 import cv2
@@ -6,9 +7,23 @@ from ultralytics import YOLO
 import tempfile
 import os
 import math
+from typing import List
+import json
 
 # YOLO 모델 로드
 model = YOLO("yolov8n.pt")
+    
+class BoxData(BaseModel):
+    id: int
+    class_name: str
+    xmin: int
+    ymin: int
+    xmax: int
+    ymax: int
+    
+class CreateVideoRequest(BaseModel):
+    target_obj_list: List[str]
+    box_data: List[BoxData]
 
 app = FastAPI()
 
@@ -22,10 +37,10 @@ async def get_inference_data(file: UploadFile):
         cap = cv2.VideoCapture(temp_file.name)
         fps = cap.get(cv2.CAP_PROP_FPS)  # FPS 값을 가져옵니다.
         frame_count = 0
-        frame_skip = 1
+        frame_skip = 10
 
         label_list = set()
-        formatted_detections = []
+        result_list = []
         object_timeline = {}
 
         while True:
@@ -40,6 +55,7 @@ async def get_inference_data(file: UploadFile):
                         boxes = result.boxes.xyxy
                         classes = result.boxes.cls
                         ids = result.boxes.id
+                        detections = []
                         for box, cls, obj_id in zip(boxes, classes, ids):
                             x1, y1, x2, y2 = box
                             label = model.model.names[int(cls)]
@@ -52,13 +68,14 @@ async def get_inference_data(file: UploadFile):
                                 "ymax": y2.item(),
                             }
                             label_list.add(label)
-                            formatted_detections.append(formatted_detection)
+                            detections.append(formatted_detection)
                             # 객체의 등장 시간과 퇴장 시간을 기록합니다.
                             obj_id_int = int(obj_id)  # 문자열로 변환하여 사용
                             if obj_id_int not in object_timeline:
                                 object_timeline[obj_id_int] = {"start": math.floor(current_time), "end": math.floor(current_time)}
                             else:
                                 object_timeline[obj_id_int]["end"] = math.ceil(current_time)
+                        result_list.append(detections)
                 except cv2.error as e:
                     print(f"OpenCV error during tracking: {e}")
                     continue
@@ -71,5 +88,22 @@ async def get_inference_data(file: UploadFile):
             cap.release()
         os.unlink(temp_file.name)
         
-    det_result = {"formatted_detections": formatted_detections, "timeline": object_timeline}
+    det_result = {"result_list": result_list, "timeline": object_timeline}
     return {"label_list": list(label_list), "det_result": det_result}
+
+# @app.post("/result-video/")
+# async def create_det_video(
+#     file: UploadFile = File(...),
+#     request: str = Form(...),
+# ):
+#     try:
+#         # JSON 문자열을 Pydantic 모델로 변환
+#         parsed_request = CreateVideoRequest(**json.loads(request))
+#     except json.JSONDecodeError:
+#         raise HTTPException(status_code=400, detail="Invalid JSON format")
+    
+#     # parsed_request를 사용하여 필요한 작업을 수행합니다.
+#     print(parsed_request)
+    
+#     # 예시: 성공 응답
+#     return {"status": "success", "parsed_request": parsed_request}
