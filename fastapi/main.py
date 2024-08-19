141a897fa0a703da26e7aafa4cce98d769bb06a8
ahmin030 <ahmin030@naver.com>
2024-08-19 15:23:47 +0900

    Feat: Tracking error clear

--- fastapi/main.py
+++ fastapi/main.py
@@ -13,7 +13,8 @@ import json
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse, JSONResponse
 import ffmpeg
-
+from datetime import timedelta
+from collections import defaultdict
 
 
 
@@ -21,13 +22,30 @@ import ffmpeg
 model = YOLO("yolov8n.pt")
 
 app = FastAPI()
+# CORS 설정 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 또는 ["*"]로 모든 도메인 허용
+    allow_origins=["*"],  # 모든 도메인에서 요청 허용
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # 모든 HTTP 메소드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
 )
+def format_time(seconds):
+    # 밀리세컨드를 계산합니다.
+    millis = round((seconds % 1) * 1000)  # 밀리세컨드 계산 및 반올림
+
+    # timedelta를 이용해 시간을 시, 분, 초로 변환합니다.
+    time_delta = timedelta(seconds=int(seconds))
+    hours, remainder = divmod(time_delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    # 밀리세컨드를 2자리로 포맷합니다.
+    millis = int(millis / 10)  # 밀리세컨드를 2자리로 변환
+
+    # 형식을 맞추기 위해 시, 분, 초를 포맷합니다.
+    formatted_time = f"{hours:02}:{minutes:02}:{seconds:02}.{millis:02}"
+
+    return formatted_time
 
 @app.post("/uploadfile/")
 async def get_inference_data(file: UploadFile):
@@ -43,13 +61,14 @@ async def get_inference_data(file: UploadFile):
 
         label_list = set()
         result_list = []
-        object_timeline = {}
+        object_timeline = defaultdict(list)  # 각 객체에 대해 여러 타임라인을 저장할 수 있도록 리스트로 변경
 
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
             current_time = frame_count / fps  # 현재 프레임의 시간을 초 단위로 계산
+            formatted_time = format_time(current_time)  # hh:mm:ss:ms 형식으로 변환
             if frame_count % frame_skip == 0:
                 try:
                     results = model.track(frame, persist=True)
@@ -72,12 +91,29 @@ async def get_inference_data(file: UploadFile):
                                 }
                                 label_list.add(label)
                                 detections.append(formatted_detection)
-                                # 객체의 등장 시간과 퇴장 시간을 기록합니다.
+                                
                                 obj_id_int = int(obj_id)
-                                if obj_id_int not in object_timeline:
-                                    object_timeline[obj_id_int] = {"label": label, "start": math.floor(current_time), "end": math.floor(current_time)}
+                                if object_timeline[obj_id_int]:
+                                    last_timeline = object_timeline[obj_id_int][-1]
+                                    if last_timeline["label"] != label:
+                                        # 라벨이 변경되었을 경우, 이전 타임라인을 종료하고 새로운 타임라인을 시작합니다.
+                                        last_timeline["end"] = formatted_time
+                                        # 새로운 타임라인 시작
+                                        object_timeline[obj_id_int].append({
+                                            "label": label, 
+                                            "start": formatted_time, 
+                                            "end": formatted_time
+                                        })
+                                    else:
+                                        # 라벨이 변경되지 않은 경우, 기존 타임라인의 종료 시간만 업데이트합니다.
+                                        last_timeline["end"] = formatted_time
                                 else:
-                                    object_timeline[obj_id_int]["end"] = math.ceil(current_time)
+                                    # 새로운 객체에 대해 타임라인을 시작합니다.
+                                    object_timeline[obj_id_int].append({
+                                        "label": label, 
+                                        "start": formatted_time, 
+                                        "end": formatted_time
+                                    })
                         result_list.append(detections)
                 except cv2.error as e:
                     print(f"OpenCV error during tracking: {e}")
@@ -94,15 +130,18 @@ async def get_inference_data(file: UploadFile):
     # 타임라인 포멧팅
     format_timeline = []
 
-    for key, value in object_timeline.items():
-        format_timeline.append({
-            'id': key,
-            'label': value['label'],
-            'start': value['start'],
-            'end': value['end']
-        })
+    for key, timelines in object_timeline.items():
+        for timeline in timelines:
+            format_timeline.append({
+                'id': key,
+                'label': timeline['label'],
+                'start': timeline['start'],
+                'end': timeline['end']
+            })
+    print(format_timeline)
     
     return {"label_list": list(label_list), "timeline": format_timeline, "det_result": result_list}
+
 
 @app.post("/result-video/")
 async def create_det_video(
@@ -134,6 +173,10 @@ async def create_det_video(
         # 프레임 관리
         frame_count = 0
         frame_skip = 1
+        
+        # 트랙킹 점 설정
+        track_history = defaultdict(lambda: [])
+        max_point = 10
 
         while True:
             # 프레임 읽기
@@ -148,6 +191,21 @@ async def create_det_video(
                     x1, x2, y1, y2 = int(obj['xmin']), int(obj['xmax']), int(obj['ymin']), int(obj['ymax'])
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                     cv2.putText(frame, f'id: {obj["id"]} class: {obj["class_name"]}', (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
+                    
+                    # 중심점 계산
+                    center_x = int(x1 + (x2 - x1) / 2)
+                    center_y = int(y1 + (y2 - y1) / 2)
+                    
+                    # 추적 경로 업데이트
+                    track_history[obj['id']].append((center_x, center_y))
+                    if len(track_history[obj['id']]) > max_point:
+                        track_history[obj['id']].pop(0)
+                    
+                    # 추적 경로 그리기
+                    points = np.array(track_history[obj['id']], dtype=np.int32)
+                    points = points.reshape((-1, 1, 2))
+                    cv2.polylines(frame, [points], isClosed=False, color=(0, 255, 0), thickness=5, lineType=cv2.LINE_AA)
+
 
             out.write(frame)
             frame_count += 1
